0875a40739af2df7990536766824c4e49cda3d87
Alyssa <alyssa@teepublic.com>
2021-04-29 16:58:45 -0400

    add safe search endpoint

--- lib/swagger_client/configuration.rb
+++ lib/swagger_client/configuration.rb
@@ -10,7 +10,7 @@ Swagger Codegen version: 2.4.19
 
 =end
 
-require 'uri'
+require 'addressable/uri'
 
 module SwaggerClient
   class Configuration
@@ -175,7 +175,7 @@ module SwaggerClient
 
     def base_url
       url = "#{scheme}://#{[host, base_path].join('/').gsub(/\/+/, '/')}".sub(/\/+\z/, '')
-      URI.encode(url)
+      Addressable::URI.encode(url)
     end
 
     # Gets API key (with prefix if set).
