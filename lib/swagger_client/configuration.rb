734af30c21d9f4eab3a0a920ad0aaf2d1f6a1725
Alyssa <alyssa@teepublic.com>
2021-05-03 14:40:02 -0400

    remove autogenerated addressable change

--- lib/swagger_client/configuration.rb
+++ lib/swagger_client/configuration.rb
@@ -175,7 +175,7 @@ module SwaggerClient
 
     def base_url
       url = "#{scheme}://#{[host, base_path].join('/').gsub(/\/+/, '/')}".sub(/\/+\z/, '')
-      Addressable::URI.encode(url)
+      URI.encode(url)
     end
 
     # Gets API key (with prefix if set).
